c93ec701e0fa1b607f216498120ccc7ccc16a15f
Aritra Das <aritrarick2002@gmail.com>
2023-06-29 20:09:39 +0530

    Home Screen Done ✅.

---
+++ app/src/main/java/com/aritra/notify/screens/homeScreen/HomeScreenViewModel.kt
@@ -0,0 +1,31 @@
+package com.aritra.notify.screens.homeScreen
+
+import android.app.Application
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.aritra.notify.data.models.Note
+import com.aritra.notify.data.repository.NoteRepository
+import kotlinx.coroutines.launch
+
+class HomeScreenViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val noteRepository = NoteRepository(application)
+    var notesModel by mutableStateOf(emptyList<Note>())
+
+    fun getAllNotes() {
+        viewModelScope.launch {
+            noteRepository.getAllNotesFromRoom().collect { response ->
+                notesModel = response
+            }
+        }
+    }
+
+    fun deleteNote(note: Note) {
+        viewModelScope.launch {
+            noteRepository.deleteNoteFromRoom(note)
+        }
+    }
+}
