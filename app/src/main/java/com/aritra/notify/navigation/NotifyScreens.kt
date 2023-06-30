aa210265aa10c4e1d06c5ba9215d5e3f3661e466
Aritra Das <aritrarick2002@gmail.com>
2023-06-30 22:03:38 +0530

    Added Bottom Navigation Bar.

--- app/src/main/java/com/aritra/notify/navigation/NotifyScreens.kt
+++ app/src/main/java/com/aritra/notify/navigation/NotifyScreens.kt
@@ -5,5 +5,6 @@ sealed class NotifyScreens(val name: String) {
     object Home : NotifyScreens("home")
     object AddNotes : NotifyScreens("add_note")
     object UpdateNotes : NotifyScreens("update_note")
+    object TodoHome : NotifyScreens("todo_home")
     object Settings : NotifyScreens("setting")
 }
