ad02dbf30a2e6f9a167f14df53a41fb575b9de67
Aritra Das <aritrarick2002@gmail.com>
2023-07-06 00:40:00 +0530

    Added Search bar.

--- app/src/main/java/com/aritra/notify/screens/notes/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/aritra/notify/screens/notes/homeScreen/HomeScreen.kt
@@ -15,12 +15,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -32,6 +32,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Surface
 import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
@@ -39,23 +40,21 @@ import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.airbnb.lottie.compose.LottieAnimation
-import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.rememberLottieComposition
 import com.aritra.notify.R
 import com.aritra.notify.components.TopBar
 import com.aritra.notify.data.models.Note
@@ -94,18 +93,35 @@ fun HomeScreen(
             modifier = Modifier.padding(it),
             color = MaterialTheme.colorScheme.onSecondary
         ) {
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(0.dp, 5.dp, 0.dp, 0.dp)
-            ) {
-                if (notesModel.isNotEmpty()) {
-                    items(notesModel) { notesModel ->
-                        SwapDelete(notesModel, viewModel, navigateToUpdateNoteScreen)
-                    }
-                } else {
-                    item {
-                        NoList()
+            var searchQuery by rememberSaveable { mutableStateOf("") }
+            Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+
+                SearchBar(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    query = searchQuery,
+                    onQueryChange = { searchQuery = it },
+                    onSearch = {},
+                    active = false,
+                    onActiveChange = {},
+                    placeholder = { Text(stringResource(R.string.search_your_notes)) },
+                    leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) }
+                ) {
+                }
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(0.dp, 5.dp, 0.dp, 0.dp)
+                ) {
+
+                    if (notesModel.isNotEmpty()) {
+                        items(notesModel) { notesModel ->
+                            SwapDelete(notesModel, viewModel, navigateToUpdateNoteScreen)
+                        }
+                    } else {
+                        item {
+                            NoList()
+                        }
                     }
                 }
             }
@@ -183,7 +199,6 @@ fun NotesCard(
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(12.dp)
         ) {
             Text(
                 text = noteModel.title,
