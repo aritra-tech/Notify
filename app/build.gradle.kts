b208011d3e6b5569fe73bf0c7796ddaf5f9b9b34
Aritra Das <aritrarick2002@gmail.com>
2023-07-29 17:20:03 +0530

    Removed Redundant dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,6 @@ dependencies {
     implementation ("androidx.compose.ui:ui-graphics")
     implementation ("androidx.compose.ui:ui-tooling-preview")
     implementation ("androidx.compose.material3:material3")
-    implementation ("androidx.constraintlayout:constraintlayout-compose-android:1.1.0-alpha10")
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
