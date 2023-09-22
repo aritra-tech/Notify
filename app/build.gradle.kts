3068110915cf7c120b68796f19897d3125a69360
Aritra Das <aritrarick2002@gmail.com>
2023-09-22 20:16:57 +0530

    Bumped `VersionName` & `VersionCode`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.aritra.notify"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.3.0-beta01"
+        versionCode = 5
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
