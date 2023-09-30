7323d6f57bde1d61cb03f23a4203a86c666009f8
Yogesh Bansal <54579951+yogesh-7@users.noreply.github.com>
2023-09-30 20:28:40 +0530

    improve the share util codes

--- app/src/main/java/com/aritra/notify/utils/CommonExtensions.kt
+++ app/src/main/java/com/aritra/notify/utils/CommonExtensions.kt
@@ -10,3 +10,21 @@ fun Context.toast(message: String) {
 fun Context.closeApp() {
     (this as? MainActivity)?.finish()
 }
+
+/**
+ * Executes the specified [block] lambda if the receiver value is not null.
+ *
+ * @param block A lambda expression that takes a non-null receiver value (`T`) as its argument and returns a result of type `R`.
+ * @return If the receiver value (`this`) is not null, the [block] lambda is executed with the non-null value, and the result is returned as a non-null value of type `R`. If the receiver value is null, the function returns null.
+ *
+ * @sample
+ * val result: Int? = "42".withNotNull { it.toIntOrNull() } // result will be 42
+ * val nullResult: Int? = null.withNotNull { it.toIntOrNull() } // nullResult will be null
+ *
+ * @param T The type of the receiver value (nullable).
+ * @param R The type of the result value (nullable).
+ * @receiver The nullable receiver value on which this function is called.
+ */
+inline fun <T : Any, R> T?.withNotNull(block: (T) -> R): R? {
+    return this?.let(block)
+}
