7f3fe440bdae74073ba278c855706fe883caa07f
Aritra Das <aritrarick2002@gmail.com>
2023-10-07 01:26:34 +0530

    Removed `un-used` code and updated some icons

--- app/src/main/java/com/aritra/notify/components/topbar/SelectionModeTopAppBar.kt
+++ app/src/main/java/com/aritra/notify/components/topbar/SelectionModeTopAppBar.kt
@@ -1,8 +1,8 @@
 package com.aritra.notify.components.topbar
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.DeleteOutline
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -19,7 +19,7 @@ fun SelectionModeTopAppBar(selectedItems: List<Int>, onDeleteClick: () -> Unit, 
             onClick = resetSelectionMode
         ) {
             Icon(
-                imageVector = Icons.Rounded.Close,
+                imageVector = Icons.Outlined.Close,
                 contentDescription = null,
                 tint = MaterialTheme.colorScheme.onBackground
             )
@@ -34,50 +34,10 @@ fun SelectionModeTopAppBar(selectedItems: List<Int>, onDeleteClick: () -> Unit, 
     }, actions = {
         IconButton(onDeleteClick) {
             Icon(
-                imageVector = Icons.Filled.Delete,
+                imageVector = Icons.Outlined.DeleteOutline,
                 contentDescription = null,
                 tint = MaterialTheme.colorScheme.onBackground
             )
         }
     })
-
-    /*Row(modifier = Modifier.fillMaxWidth().padding(8.dp), verticalAlignment = Alignment.CenterVertically) {
-        Icon(
-            imageVector = Icons.Rounded.Close,
-            contentDescription = null,
-            tint = MaterialTheme.colorScheme.onBackground,
-            modifier = Modifier.clickable {
-                resetSelectionMode()
-            }
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-
-        Text(
-            text = "${selectedItems.size} selected",
-            style = MaterialTheme.typography.titleMedium.copy(
-                color = MaterialTheme.colorScheme.onBackground,
-            ),
-        )
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        Icon(
-            imageVector = Icons.Filled.Delete,
-            contentDescription = null,
-            tint = MaterialTheme.colorScheme.onBackground,
-            modifier = Modifier.clickable {
-                onDeleteClick()
-            }
-        )
-    }*/
 }
-
-/*
-@Preview(showBackground = true)
-@Composable
-fun Preview() {
-    SelectionModeTopAppBar(selectedItems = emptyList()) {
-
-    }
-}
-*/
