de0b52c2621fa074301b39e4422e75623df84ae0
utkarsh <utkarshsinghrajput16@gmail.com>
2023-10-18 05:06:31 +0530

    fix: Clickable Animation Issues in Bottom Navigation Item

--- app/src/main/java/com/aritra/notify/navigation/NotifyApp.kt
+++ app/src/main/java/com/aritra/notify/navigation/NotifyApp.kt
@@ -151,9 +151,12 @@ fun BottomNavigationBar(
                                 lazyListState.animateScrollToItem(0)
                             }
                         }
-                        navController.navigate(item.route) {
-                            popUpTo(navController.graph.startDestinationId)
-                            launchSingleTop = true
+                        if (backStackEntry.value?.destination?.route != item.route)
+                        {
+                            navController.navigate(item.route) {
+                                popUpTo(navController.graph.startDestinationId)
+                                launchSingleTop = true
+                            }
                         }
                     }
                 )
