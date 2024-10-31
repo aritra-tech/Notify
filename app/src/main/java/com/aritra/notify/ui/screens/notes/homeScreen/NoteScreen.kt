6fc1195fc5d00e97e503e0c99c3f42bcfa781914
Ifeoluwa Andrew Omole <iomole3@gmail.com>
2024-10-31 10:25:06 +0100

    Removed delete button from top app bar

--- app/src/main/java/com/aritra/notify/ui/screens/notes/homeScreen/NoteScreen.kt
+++ app/src/main/java/com/aritra/notify/ui/screens/notes/homeScreen/NoteScreen.kt
@@ -129,7 +129,7 @@ fun SharedTransitionScope.NoteScreen(
             if (isInSelectionMode) {
                 SelectionModeTopAppBar(
                     selectedItems = selectedNoteIds,
-                    onDeleteClick = {
+                    onSelectAllClick = {
                         val selectedNotes =
                             listOfAllNotes.filter { note -> note.id in selectedNoteIds }
 
